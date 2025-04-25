cb9b293c3285c8a7bb925426d4118206720a0317
Dylan-M-A <darmentorbusiness@gmail.com>
2025-04-25 07:05:08 -0500

    files updating

--- CDDS_Optimise/main.cpp
+++ CDDS_Optimise/main.cpp
@@ -78,6 +78,7 @@ int main(int argc, char* argv[])
         //----------------------------------------------------------------------------------
 
         float delta = GetFrameTime();
+        SetTargetFPS(60);
 
         // update the destroyer
         destroyer.Update(delta);
